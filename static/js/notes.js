6593f2ffc8505569b9309ec901808b75843f4960
raingxm <raingxm@163.com>
2015-08-21 13:20:44 +0800

    change style of upload button

--- static/js/notes.js
+++ static/js/notes.js
@@ -2,7 +2,11 @@
 
     $(document).ready(function() {
         $('.describe .like').click(function() {
-        $(this).toggleClass('active');
-    });
+            $(this).toggleClass('active');
+        });
+
+        $('#id_image').addClass('custom-file-input');
+
+        $('#id_photo').addClass('custom-file-input');
     });
 })();
