d01d4e70c4a390ecc5ef0beac10cbba35e505ca1
raingxm <raingxm@163.com>
2015-08-21 14:16:17 +0800

    change use slow speed

--- static/js/notes.js
+++ static/js/notes.js
@@ -3,8 +3,8 @@
         $('.title .like').click(function() {
             $(this).toggleClass('active');
             if($(this).hasClass('active')) {
-                $(".panel-body .animation-number").fadeIn();
-                $(".panel-body .animation-number").fadeOut();
+                $(".panel-body .animation-number").fadeIn('slow');
+                $(".panel-body .animation-number").fadeOut('slow');
             }
 
         });
